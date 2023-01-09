91d4e797bd3054fa2b08179a3cd0c19abdd779a3
jeya <jeyamanikrishnakumar@gmail.com>
2023-01-09 19:12:07 +0530

    new code accepted

---
+++ src/org/loop/Loop.java
@@ -0,0 +1,19 @@
+package org.loop;
+
+public class Loop {
+	public static void main(String[] args) {
+		int a=10;  //assign
+		if(a==10) {
+			//10=10 true
+			System.out.println("a is 10");
+		}
+		int b=20;
+		if(b==10) {
+			//20=10 false
+			System.out.println("b is 20");
+		}
+	
+	} 
+	
+
+}
